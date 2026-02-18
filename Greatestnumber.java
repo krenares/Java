5859c7ac49c4b8884e9f73999283d41da7274d0c
Krenare Syla <55036085+krenares@users.noreply.github.com>
2026-02-18 14:39:43 -0600

    Update Greatestnumber.java

--- Greatestnumber.java
+++ Greatestnumber.java
@@ -5,7 +5,9 @@ class Main {
             System.out.println(number1 + " is the greatest number.");
         else if (number2 >= number1 && number2 >= number3)
             System.out.println(number2 + " is the greatest number.");
+          
         else
             System.out.println(number3 + " is the greatest number.");
     }
+
 }
